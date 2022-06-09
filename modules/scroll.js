7f3b70634787009da24cf436f3db3243dd59b437
Herman <smurovnemez@mail.ru>
2022-06-09 06:50:05 +0300

    add-scroll

--- modules/scroll.js
+++ modules/scroll.js
@@ -1,4 +1,25 @@
-const navbar = document.querySelector('.header__nav')
-const links = navbar.querySelectorAll('a')
+    const navbar = document.querySelector('.header__nav')
+    const links = navbar.querySelectorAll('a')
 
-console.log(links)
+    links.forEach((link) => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault()
+
+
+            const section = document.querySelector(link.getAttribute('href'))
+
+            if (section) {
+                seamless.scrollIntoView(section, {
+                    behavior: "smooth",
+                    block: "start",
+                    inline: "center",
+                });
+
+            }
+
+
+        })
+
+
+
+    })
